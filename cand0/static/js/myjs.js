a5ff371af9749efa5aa319381e1c620dea7568be
cand0 <cand0.gmail.com>
2020-03-25 18:56:11 +0900

    enable line breaks on the page

--- cand0/static/js/myjs.js
+++ cand0/static/js/myjs.js
@@ -1,7 +1,15 @@
-function challenge_choice_on(challenge_name, challenge_message, challenge_value){
-	document.getElementById("challenge_name").innerHTML = challenge_name;
-	document.getElementById("challenge_message").innerHTML = challenge_message;
+function challenge_choice_on(challenge){
+	challenge_name = challenge + "name";
+	challenge_message = challenge + "message";
 
+	challenge_name = document.getElementById(challenge_name).innerHTML;
+	challenge_message = document.getElementById(challenge_message).innerHTML;
+
+	challenge_name = challenge_name.replace(/(?:\r\n|\r|\n)/g, '<br />');
+	document.getElementById("challenge_name").innerHTML = challenge_name;
+
+	challenge_message = challenge_message.replace(/(?:\r\n|\r|\n)/g, '<br />');
+	document.getElementById("challenge_message").innerHTML = challenge_message;
 	document.getElementById("challenge-choice-wrapper").style.zIndex = "1";
 	document.getElementById("challenge-choice-body").style.zIndex = "2";
 }
